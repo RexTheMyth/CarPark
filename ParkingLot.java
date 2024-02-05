92e70a30d460ca441dfe0d238032fd0738713909
RexTheMyth <rexthemyth@gmail.com>
2024-02-04 23:37:56 -0500

    second commit

--- ParkingLot.java
+++ ParkingLot.java
@@ -4,7 +4,7 @@ public class ParkingLot {
     public ParkingLot(){
         this.spots = new ParkingSpot[10];
         for (int i = 0; i < 10; i++){
-            if (i > 2){
+            if (i > 1){
                 this.spots[i] = new ParkingSpot(false);
             }
             else{
@@ -17,8 +17,60 @@ public class ParkingLot {
 
     public String toString(){
 
-        return"";
+        int handiSpots = 0;
+
+        int unHandiSpots = 0;
+
+        for (int i = 0; i < 2; i++){
+            if (this.spots[i].parkedCar == null){
+                handiSpots++;
+            } 
+        }
+
+        for (int i = 2; i < 10; i++){
+            if (this.spots[i].parkedCar == null){
+                unHandiSpots++;
+            }
+
+        }
+
+        return handiSpots + " " + unHandiSpots;
 
     }
     
+    public int park(Car car){
+
+        if (car.handicap == false){
+            for (int i = 2; i < 10; i++){
+                if (this.spots[i].parkedCar == null){
+                    this.spots[i].parkedCar = car;
+
+                    return i;
+                }
+            }
+
+        }
+
+        else{
+            for (int i = 0; i < 10; i++){
+                if (this.spots[i].parkedCar == null){
+                    this.spots[i].parkedCar = car;
+
+                    return i;
+                }
+            }
+
+        }
+
+        return 10;
+    }
+
+    public Car unpark(int index){
+
+        Car unParkedCar = this.spots[index].parkedCar;
+
+        this.spots[index].parkedCar = null;
+
+        return unParkedCar;
+    }
 }
--- ParkingTester.java
+++ ParkingTester.java
@@ -1,6 +1,25 @@
 public class ParkingTester{
     public static void main(String[] args) {
-        new ParkingLot();
-        
+
+        ParkingLot parkingLot = new ParkingLot();
+
+        System.out.println(parkingLot.toString());
+
+        Car firstCar = new Car("Infiniti", "A", true);
+
+        parkingLot.park(firstCar);
+
+        System.out.println(parkingLot.toString());
+
+        Car secondCar = new Car("Cadiliac", "B", false);
+
+        parkingLot.park(secondCar);
+
+        System.out.println(parkingLot.toString());
+
+        parkingLot.unpark(0);
+
+        System.out.println(parkingLot.toString());
+
     }
 }
